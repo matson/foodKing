bf8a27e8a24c1e7b25cfdc5ef80c3b7b3f8a2c0e
Madison <madison.k.adams@gmail.com>
2023-04-26 16:05:57 -0400

    starter project

---
+++ foodKing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodKing
+//
+//  Created by Tracy Adams on 4/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
