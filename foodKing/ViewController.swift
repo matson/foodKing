cda7945355a8848d89f1db135a0c6effac324d5c
Madison <madison.k.adams@gmail.com>
2023-04-26 16:33:43 -0400

    add food features

--- foodKing/ViewController.swift
+++ foodKing/ViewController.swift
@@ -13,7 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    //foods to chose from:
+    //Italian
+    //Japanese
+    //Mexican
+    //American
+    
 
 }
 
